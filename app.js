6c7ca68b3c5f1e5ad5a9dac29a1cdfbf5d1d3884
Alden Quimby <aldenquimby@gmail.com>
2013-09-16 02:23:41 -0400

    type

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
 
 var express = require('express');
